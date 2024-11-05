5f44594bac3cd8ce1e020fa85bae2ebc745badef
tuanhuynh0209 <135773362+tuanhuynh0209@users.noreply.github.com>
2024-11-05 15:56:31 +0700

    thêm sản phẩm

--- src/pages/AddScientificResPro.jsx
+++ src/pages/AddScientificResPro.jsx
@@ -1,16 +1,40 @@
 import React, { useCallback, useEffect, useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
+import SuccessDialog from '../Dialog/SuccessDialog';
+import axios from 'axios';
 const AddScientificResPro = () => {
-
-    const [activity, setActivity] = useState();
-    const [scope, setScope] = useState();
-    const [standardTime, setStandardTime] = useState();
-    const [contributionRate, setContributionRate] = useState();
-    const [timeRoleTemp, setTimeRoleTemp] = useState();
+    const [open, setOpen] = useState(false);
+    const navigate = useNavigate();
+    // Hàm đóng dialog và điều hướng
+    const handleClose = () => {
+        setOpen(false);
+        navigate('/func/scientificResearchProduct'); // Chuyển đến trang bạn muốn
+    };
+    const [formData, setFormData] = useState({
+        msnv: 0,
+        hoat_dong: '',
+        ten_san_pham: '',
+        don_vi_cap_chung_nhan: '',
+        minh_chung: '',
+        pham_vi: '',
+        ngay: '',
+        gio_chuan_hoat_dong: '',
+        ty_le_dong_gop: '',
+        gio_quy_doi: ''
+    });
+    useEffect(() => {
+        const storeMsnv = localStorage.getItem('userId');
+        if (storeMsnv) {
+            setFormData((prevData) => ({
+                ...prevData,
+                msnv: storeMsnv,
+            }));
+        }
+    }, []);
 
     const calculateStandardHours = useCallback(() => {
         let hours = 0;
-        switch (activity) {
+        switch (formData.hoat_dong) {
             case "Giấy chứng nhận sáng chế độc quyền (patent)":
                 hours = 200;
                 break;
@@ -27,24 +51,40 @@ const AddScientificResPro = () => {
                 hours = 0;
         }
         let numScope = 0;
-        if (scope === "Trong nước") {
+        if (formData.pham_vi === "Trong nước") {
             numScope = 1;
-        } else if (scope === "Quốc tế") {
+        } else if (formData.pham_vi === "Quốc tế") {
             numScope = 2;
         }
         const numStandandHours = hours * numScope
-        setStandardTime(numStandandHours);
-    }, [activity, scope]);
-    const calculateStandardHoursTemp = useCallback(() => {
-        let numContribute = parseFloat(contributionRate);
-        const hoursTemp = standardTime * numContribute;
-        setTimeRoleTemp(hoursTemp);
-    }, [contributionRate, standardTime]);
-
+        return numStandandHours;
+    }, [formData.hoat_dong, formData.pham_vi]);
     useEffect(() => {
-        calculateStandardHours();
-        calculateStandardHoursTemp();
-    }, [calculateStandardHours, calculateStandardHoursTemp]);
+        const calculatedHours = calculateStandardHours();
+        setFormData(prevData => ({ ...prevData, gio_chuan_hoat_dong: calculatedHours }));
+    }, [formData.hoat_dong, formData.pham_vi, calculateStandardHours]);
+
+    const calculateStandardHoursTemp = useCallback(() => {
+        let numContribute = parseFloat(formData.ty_le_dong_gop);
+        let numHours = parseFloat(formData.gio_chuan_hoat_dong);
+        const hoursTemp = numHours * numContribute;
+        return hoursTemp;
+    }, [formData.ty_le_dong_gop, formData.gio_chuan_hoat_dong]);
+    useEffect(() => {
+        const calculatedStandardHoursTemp = calculateStandardHoursTemp();
+        setFormData(prevData => ({ ...prevData, gio_quy_doi: calculatedStandardHoursTemp }));
+    }, [formData.ty_le_dong_gop, formData.gio_chuan_hoat_dong, calculateStandardHoursTemp]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await axios.post('http://localhost:3001/education/AddSciResPro', formData);
+            console.log('Tài liệu đã được thêm:', response.data);
+            setOpen(true);  // Hiển thị dialog khi thêm thành công
+        } catch (error) {
+            console.error('Lỗi khi thêm tài liệu:', error);
+        }
+    };
 
     return (
         <div className='mx-8 w-full'>
@@ -52,7 +92,7 @@ const AddScientificResPro = () => {
                 <span className='text-3xl font-bold'>Đăng ký sở hữu trí tuệ, triển lãm sản phẩm khoa học công nghệ</span>
                 <hr className='my-4 border-gray-300' />
             </div>
-            <div className='w-full h-full p-10 bg-white shadow-lg rounded-lg'>
+            <form onSubmit={handleSubmit} className='w-full h-full p-10 bg-white shadow-lg rounded-lg'>
                 <div className="flex flex-col gap-6">
 
                     {/* Employee ID (to be passed from user data) */}
@@ -60,22 +100,19 @@ const AddScientificResPro = () => {
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Mã số nhân viên</p>
                         </div>
-                        <input type="text" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" disabled placeholder="Mã số nhân viên" />
-                    </div>
-
-                    {/* Full Name (to be passed from user data) */}
-                    <div className="flex flex-col gap-1">
-                        <div className="flex gap-2 items-center">
-                            <p className='font-medium text-lg'>Họ và tên</p>
-                        </div>
-                        <input type="text" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" disabled placeholder="Họ và tiên viên chức" />
+                        <input
+                            type="text"
+                            value={formData.msnv}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            disabled placeholder="Mã số nhân viên" />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Hoạt động đã đạt</p>
                         </div>
-                        <select className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" onChange={(e) => { setActivity(e.target.value); calculateStandardHours() }}>
+                        <select className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            onChange={(e) => setFormData({ ...formData, hoat_dong: e.target.value })}>
                             <option value="">Ấn vào để chọn</option>
                             <option value="Giấy chứng nhận sáng chế độc quyền (patent)">Giấy chứng nhận sáng chế độc quyền (patent)</option>
                             <option value="Giấy chứng nhận giải pháp hữu ích">Giấy chứng nhận giải pháp hữu ích</option>
@@ -88,28 +125,41 @@ const AddScientificResPro = () => {
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Tên sản phẩm, giải pháp, nhãn hiệu đã được cấp chứng nhận hoặc giới thiệu tại hội chợ, triển lãm</p>
                         </div>
-                        <input type="text" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder="Nhập thông tin" />
+                        <input
+                            type="text"
+                            onChange={(e) => setFormData({ ...formData, ten_san_pham: e.target.value })}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            placeholder="Nhập thông tin" />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Đơn vị cấp chứng nhận, tổ chức</p>
                         </div>
-                        <input type="text" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder="Nhập đơn vị" />
+                        <input
+                            type="text"
+                            onChange={(e) => setFormData({ ...formData, don_vi_cap_chung_nhan: e.target.value })}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            placeholder="Nhập đơn vị" />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Minh chứng</p>
                         </div>
-                        <input type="text" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder="Nhập minh chứng" />
+                        <input
+                            type="text"
+                            onChange={(e) => setFormData({ ...formData, minh_chung: e.target.value })}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            placeholder="Nhập minh chứng" />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Phạm vi</p>
                         </div>
-                        <select className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" onChange={(e) => { setScope(e.target.value); calculateStandardHours() }}>
+                        <select className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
+                            onChange={(e) => setFormData({ ...formData, pham_vi: e.target.value })}>
                             <option value="">Ấn vào để chọn</option>
                             <option value="Trong nước">Trong nước</option>
                             <option value="Quốc tế">Quốc tế</option>
@@ -120,14 +170,21 @@ const AddScientificResPro = () => {
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Ngày</p>
                         </div>
-                        <input type="date" className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" />
+                        <input
+                            type="date"
+                            onChange={(e) => setFormData({ ...formData, ngay: e.target.value })}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Giờ chuẩn hoạt động</p>
                         </div>
-                        <input type="text" readOnly value={isNaN(standardTime) ? '' : standardTime} className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder='Nhập giờ chuẩn' />
+                        <input
+                            type="text"
+                            readOnly
+                            value={isNaN(formData.gio_chuan_hoat_dong) ? '' : formData.gio_chuan_hoat_dong}
+                            className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder='Nhập giờ chuẩn' />
                     </div>
 
                     <div className="flex flex-col gap-1">
@@ -137,24 +194,24 @@ const AddScientificResPro = () => {
                         <input type="text"
                             className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300"
                             placeholder='Nhập số liệu'
-                            value={contributionRate}
-                            onChange={(e) => setContributionRate(Number(e.target.value))} />
+                            onChange={(e) => setFormData({ ...formData, ty_le_dong_gop: e.target.value })} />
                     </div>
 
                     <div className="flex flex-col gap-1">
                         <div className="flex gap-2 items-center">
                             <p className='font-medium text-lg'>Giờ chuẩn quy đổi theo vai trò(tạm tính)</p>
                         </div>
-                        <input type="text" readOnly value={isNaN(timeRoleTemp) ? '' : timeRoleTemp} className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder="Nhập số giờ" />
+                        <input type="text" readOnly value={isNaN(formData.gio_quy_doi) ? '' : formData.gio_quy_doi} className="bg-slate-100 rounded-lg p-4 outline-none border border-gray-300" placeholder="Nhập số giờ" />
                     </div>
 
                     <div className='w-full flex justify-center mt-6'>
-                        <button className='bg-[#F9A150] hover:bg-[#e08f40] rounded-lg p-4 text-lg w-fit px-20 font-bold text-white outline-none transition duration-300'>
+                        <button type='submit' className='bg-[#F9A150] hover:bg-[#e08f40] rounded-lg p-4 text-lg w-fit px-20 font-bold text-white outline-none transition duration-300'>
                             Hoàn tất
                         </button>
                     </div>
                 </div>
-            </div>
+            </form>
+            <SuccessDialog open={open} onClose={handleClose} />
         </div>
     )
 }
